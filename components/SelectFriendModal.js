b591b5593fd4fee4e141336ee240bf6f46d81e87
ejuss00 <fatehshukri@gmail.com>
2023-01-12 17:30:23 +0800

    working on friendlist, update the modal pls

--- components/SelectFriendModal.js
+++ components/SelectFriendModal.js
@@ -1,21 +1,30 @@
 import React , {useState, useEffect}from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, Dimensions, FlatList } from 'react-native';
-import CheckBox from '@react-native-community/checkbox';
+import { View, Text, Image, StyleSheet, TouchableOpacity, Dimensions, FlatList, ActivityIndicator, Alert } from 'react-native';
+import Checkbox from 'expo-checkbox';
+// import CheckBox from '@react-native-community/checkbox';
 // import { FlatList } from 'react-native-gesture-handler';
 // import Ionicons from '@expo/vector-icons/Ionicons';
 
 const WIDTH_MODAL = Dimensions.get('window').width;
 const HEIGHT_MODAL = Dimensions.get('window').height;
 
+
+
+//The MAIN MAIN
 const SelectFriendModal = (props) => {
-    //modal
+    //modal ===
     const closeModal = (bool, data) => {
         props.changeModalVisibility(bool);
         props.setData(data);
     }
 
-    //checkbox
-    const [isLoading, setIsLoading] = useState(true);
+    //expo checkbox ===
+    const [isChecked, setIsChecked] = useState(false);
+
+    
+
+    //checkbox ===
+    const [isLoading, setisLoading] = useState(true);
     const [data, setData] = useState([]);
 
     useEffect(() => {
@@ -25,30 +34,73 @@ const SelectFriendModal = (props) => {
     }, []);
 
     getListPhotos = () => {
-        const apiURL = 'https://jsonplaceholder.typicode.com/posts';
+        const apiURL = 'https://jsonplaceholder.typicode.com/photos';
         fetch(apiURL)
         .then((res) => res.json())
         .then((resJson) => {
             setData(resJson);
         }).catch((error) => {
             console.log('Error: ', error)
-        })
+        }).finally(() => setisLoading(false))
     }
 
-    renderItem = ({item}) => {
+    onChangeValue = (itemSelected, index) => {
+        const newData = data.map(item => {
+            if (item.id == itemSelected.id){
+                return {
+                    ...item,
+                    selected: !item.selected
+                }
+            }
+            return {
+                ...item,
+                selected: item.selected
+            }
+        })
+        setData(newData);
+    }
+
+    renderItem = ({item, index}) => {
         return(
-            <View>
-                <Text> {item.title  } </Text>
+            <View style={styles.item}>
+                <Image
+                    style={styles.image}
+                    source={{uri: item.url}}
+                    resizeMode='contain'
+                />
+                <View style={styles.wrapText}>
+                <Text> {item.title}</Text>
+                <Checkbox
+                        // style={styles.checkBox2}
+                        // value={isChecked}
+                        // disabled={false}
+                        // tintColors={{ true: '#4630EB', false: 'gray' }} 
+                        // onValueChange={() => {
+                        //     setIsChecked(!isChecked);
+                        //     onChangeValue(item, index);}}
+                        onValueChange={() => onChangeValue(item, index)}
+                        />
+                </View>
             </View>
         )
     }
+
+    onShowItemSelected = () => {
+        const listSelected = data.filter(item => item.selected == true);
+        let contentAlert = '';
+        listSelected.forEach(item => {
+         contentAlert = contentAlert + `${item.id} . ` + item.title + ' \n';
+        })
+        Alert.alert(contentAlert);
+    }
+
 
     return (
         <TouchableOpacity
         disabled={true}
         style={styles.container}>    
             <View style={styles.modal}>
-                {/* Top Text Container========= */}
+                {/* Top Text Container === */}
               <View style={styles.textView}>
 
                 <Text style={[styles.text1, {fontSize:25, backgroundColor:'#ABCDEF', borderRadius: 50}]} >
@@ -56,35 +108,37 @@ const SelectFriendModal = (props) => {
                 <Text style={[styles.text2, {backgroundColor:'#C0D9F3'}]}>
                  Sample Sample Sample</Text>
 
-                 {/* FlatList ======= */}
-                 <FlatList 
-                 style={styles.list}
-                 data={data}
-                 renderItem={renderItem}
-                 keyExtractor ={item => `key-${item.id}`}
-
-                 />
+                 {/* FlatList === */}
+                 {isLoading ? <ActivityIndicator/> :(
+                    <FlatList 
+                        style={styles.list}
+                        data={data}
+                        renderItem={renderItem}
+                        keyExtractor ={item => `key-${item.id}`}
+                    />
+                 )}
+                 
                  <View style = {styles.wrapButton}>
-                    <TouchableOpacity style={styles.button}>
+                    <TouchableOpacity style={styles.button} onPress={onShowItemSelected}>
                         <Text>Show item you selected</Text>
                     </TouchableOpacity>
                  </View>
 
               </View>
 
-              {/* Bottom Buttons ========= */}
+              {/* Bottom Buttons === */}
               <View style={styles.buttonsView}>
                 <TouchableOpacity
                 style={styles.TouchableOpacity}
                 onPress={() => closeModal(false, 'Cancel')}
                 >
-                    <Text style={[styles.text, {color: 'blue'} ]}>Cancel</Text>
+                    <Text style={[styles.text1, {color: 'blue'} ]}>Cancel</Text>
                 </TouchableOpacity>
                 <TouchableOpacity
                 style={styles.TouchableOpacity}
                 onPress={() => closeModal(false, 'Ok')}
                 >
-                    <Text style={[styles.text, {color: 'blue'} ]}>OK</Text>
+                    <Text style={[styles.text1, {color: 'blue'} ]}>OK</Text>
                 </TouchableOpacity>
               </View>
             </View>
@@ -128,7 +182,7 @@ const styles = StyleSheet.create({
         paddingVertical: 10,
         alignItems: 'center',
     },
-    //flatlist style
+    //flatlist style ===
     list: {
         flex: 1,
         padding: 10,
@@ -137,11 +191,37 @@ const styles = StyleSheet.create({
         width: '100%',
         justifyContent: 'center',
         alignItems: 'center',
+        bottom: 20,
     },
     button: {
         padding : 16,
         backgroundColor: '#76ADE5',
+    }, 
+    item :{
+        flexDirection: 'row',
+        marginTop: 8,
+        padding: 4,
+        shadowColor: '#000',
+        shadowOffset: {width: 0, height: 2},
+        shadowOpacity: 0.25,
+        shadowRadius: 4,
+        elevation: 5,
     },
+    image: {
+        width: 100,
+        height: 150,
+    },
+    wrapText: {
+        flex: 1,
+        marginTop: 16,
+        marginLeft: 8,
+    },
+    //expo-checkbox ===
+      checkBox2: {
+        width: 15,
+        height: 15,
+        margin: 5,
+      },
 })
 
 export default SelectFriendModal;
--- screens/HFriendProfile.js
+++ screens/HFriendProfile.js
@@ -1,15 +1,92 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React , {useState}from 'react';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, Modal } from 'react-native';
+
+import { Button } from 'react-native-elements';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
+import MSelectFriends from '../components/MSelectFriends';
 
-function HFriendProfile() {
+
+export default function HFriendProfile() {
+    const [friendList, setFriendList] = useState([]);
+    const [uniqueNumber, setUniqueNumber] = useState(1);
+    const [modalVisible, setModalVisible] = useState(false);
+  
+    const addFriend = () => {
+      setFriendList([...friendList, { name: 'Friend ' + uniqueNumber }]);
+      setUniqueNumber(uniqueNumber + 1);
+    }
+  
     return (
-        <View>
-            <Text>Hello, Home Page!</Text>
-            <Ionicons name="md-checkmark-circle" size={32} color="green" />
+      <View>
+        <View style={styles.headerContainer}>
+          <Text style={styles.title}>Friend/User List</Text>
+          <Button
+            title="Add Friend"
+            onPress={() => {
+              addFriend();
+            }}
+          />
         </View>
-    );
-}
+  
+        <FlatList
+          data={friendList}
+          keyExtractor={(item) => item.name}
+          renderItem={({ item }) => (
+            <TouchableOpacity style={styles.friendContainer}>
+              <Text style={styles.friendName}>{item.name}</Text>
+            </TouchableOpacity>
+          )}
+        />
+        <View style={styles.buttonCreateBill}>
+        <TouchableOpacity
+                style={{ position: 'absolute', bottom: 20, right: 20 }}
+                onPress={() => setModalVisible(true)}
+            >
+            <Ionicons name="md-wallet" size={32} color="green" style={{ width: 40, height: 40 }}/>
+            </TouchableOpacity>
 
-export default HFriendProfile;
+            <Modal
+                animationType="slide"
+                transparent={false}
+                visible={modalVisible}
+            >
+                <MSelectFriends
+                closeModal={() => setModalVisible(false)}
+                />
+            </Modal>
+        </View>
+
+        
+      </View>
+      
+    );
+  }
+  const styles = StyleSheet.create({
+    headerContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      padding: 20,
+    },
+    title: {
+      fontWeight: 'bold',
+      fontSize: 22,
+    },
+    friendContainer: {
+      padding: 10,
+      alignItems: 'center',
+      width: '33%',
+      backgroundColor: '#4285F4',
+    },
+    friendName: {
+      fontWeight: 'bold',
+      fontSize: 18,
+    },
+    buttonCreateBill: {
+        position: 'relative',
+        backgroundColor: 'Azure',
+    }
+  });
+
+
